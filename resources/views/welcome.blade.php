073cf53d75ca6d4a264d848006e5a37e05155480
Vicky <116161445+aryasgrave@users.noreply.github.com>
2025-01-20 09:18:16 +0100

    Application setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Laravel with Vue 3</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div id="app">
+        <hello-world></hello-world>
+    </div>
+</body>
+
+</html>
